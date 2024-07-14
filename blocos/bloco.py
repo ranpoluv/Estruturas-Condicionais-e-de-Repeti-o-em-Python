33f44394cadf5d36c91c7c93534384fbfab7e15d
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-13 21:58:02 -0300

    bloco.py

---
+++ blocos/bloco.py
@@ -0,0 +1,9 @@
+def sacar(self, valor: float) ➝ None: # Início do bloco do método
+
+    if self.saldo >= valor: # início do bloco if
+
+        self.saldo -= valor
+
+    # fim do bloco do if
+
+# fim do bloco método
