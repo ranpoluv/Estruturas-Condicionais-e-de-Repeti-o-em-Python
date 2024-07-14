2d23b2918688ec771f996fcd5f25bd304d0c2f2a
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-07-13 21:57:41 -0300

    bloco.java

---
+++ blocos/bloco.java
@@ -0,0 +1,17 @@
+// Bloco em Java
+void sacar(double valor) { // início do bloco do método
+
+    if (this.saldo >= valor) { // início do bloco if
+
+    this.saldo -= valor;
+
+    } // fim do bloco do if
+
+} // fim do bloco do método
+
+// Bloco em Java sem formatar
+void sacar(double valor) { // início do bloco do método
+if (this.saldo >= valor) { // início do bloco if
+this.saldo -= valor;
+} // fim do bloco do if
+} // fim do bloco do método
